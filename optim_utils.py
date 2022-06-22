1f45d0789c6f00a7645cb0fe9a90bcbc1ab223ee
Anna <afruehstueck@users.noreply.github.com>
2022-06-22 03:54:29 +0300

    minor code updates

--- optim_utils.py
+++ optim_utils.py
@@ -12,19 +12,6 @@ from scipy.ndimage.filters import gaussian_filter1d
 
 import numpy as np
 from PIL import Image
-        
-class Images(Dataset):
-    def __init__(self, root_dir):
-        self.root_path = Path(root_dir)
-        self.image_list = sorted(list(self.root_path.glob("*.png")))
-
-    def __len__(self):
-        return len(self.image_list)
-
-    def __getitem__(self, idx):
-        img_path = self.image_list[idx]
-        image = torchvision.transforms.ToTensor()(Image.open(img_path))
-        return image,img_path.stem
 
 class BicubicDownSample(torch.nn.Module):
     def bicubic_kernel(self, x, a=-0.50):
@@ -94,76 +81,7 @@ class BicubicDownSample(torch.nn.Module):
             return x.type('torch.ByteTensor'.format(self.cuda))
         else:
             return x
-
-class GaussianSmoothing(torch.nn.Module):
-    """
-    Apply gaussian smoothing on a
-    1d, 2d or 3d tensor. Filtering is performed seperately for each channel
-    in the input using a depthwise convolution.
-    Arguments:
-        channels (int, sequence): Number of channels of the input tensors. Output will
-            have this number of channels as well.
-        kernel_size (int, sequence): Size of the gaussian kernel.
-        sigma (float, sequence): Standard deviation of the gaussian kernel.
-        dim (int, optional): The number of dimensions of the data.
-            Default value is 2 (spatial).
-    """
-    def __init__(self, channels, kernel_size, sigma, dim=2):
-        super(GaussianSmoothing, self).__init__()
-        self.kernel_size = kernel_size
-        
-        if isinstance(kernel_size, numbers.Number):
-            kernel_size = [kernel_size] * dim
-        if isinstance(sigma, numbers.Number):
-            sigma = [sigma] * dim
-        
-        # The gaussian kernel is the product of the
-        # gaussian function of each dimension.
-        kernel = 1
-        meshgrids = torch.meshgrid(
-            [
-                torch.arange(size, dtype=torch.float32)
-                for size in kernel_size
-            ]
-        )
-        for size, std, mgrid in zip(kernel_size, sigma, meshgrids):
-            mean = (size - 1) / 2
-            kernel *= 1 / (std * math.sqrt(2 * math.pi)) * \
-                      torch.exp(-((mgrid - mean) / std) ** 2 / 2)
-
-        # Make sure sum of values in gaussian kernel equals 1.
-        kernel = kernel / torch.sum(kernel)
-
-        # Reshape to depthwise convolutional weight
-        kernel = kernel.view(1, 1, *kernel.size())
-        kernel = kernel.repeat(channels, *[1] * (kernel.dim() - 1))
-
-        self.register_buffer('weight', kernel)
-        self.groups = channels
-
-        if dim == 1:
-            self.conv = F.conv1d
-        elif dim == 2:
-            self.conv = F.conv2d
-        elif dim == 3:
-            self.conv = F.conv3d
-        else:
-            raise RuntimeError(
-                'Only 1, 2 and 3 dimensions are supported. Received {}.'.format(dim)
-            )
-
-    def forward(self, input):
-        """
-        Apply gaussian filter to input.
-        Arguments:
-            input (torch.Tensor): Input to apply gaussian filter on.
-        Returns:
-            filtered (torch.Tensor): Filtered output.
-        """
-        h = self.kernel_size // 2
-        input = F.pad(input, (h, h, h, h), mode='reflect')
-        return self.conv(input, weight=self.weight, groups=self.groups)
-                 
+       
 def show_tensors(tensors, gh, gw, ax=None, normalize=True, title=None):
     images = tensors.clone() if torch.is_tensor(tensors) else tensors
     if len(images.shape) == 3:
